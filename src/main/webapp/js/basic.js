673fbca5857341fb5f68d06eef339c8e9f60b21f
skf001 <test@skf001.com>
2017-11-20 17:35:25 +0800

    第一次提交

---
+++ src/main/webapp/js/basic.js
@@ -0,0 +1,19 @@
+basePath = '';
+img_base_url= 'http://7xkkim.com1.z0.glb.clouddn.com/';
+post_cover_thumbnail='?imageView2/2/w/500';
+album_thumbnail='?imageView2/1/w/200/h/200';
+
+		var current_version = 'develop';
+var osf = {
+		develop:{
+			basePath:'',
+			post_cover_style:null,
+			album_thumbnail:null
+		},
+		release:{
+			basePath:'',
+			post_cover_style:'@!postcover',
+			album_thumbnail:'@!albumthumbnail'
+		}
+		
+}
